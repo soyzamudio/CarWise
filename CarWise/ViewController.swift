eec7bc259b551002d34b415cbf6c33a1e93f23da
Jose Zamudio <jose@fanumapp.com>
2014-10-26 18:08:58 -0400

    Initial Commit

---
+++ CarWise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarWise
+//
+//  Created by Jose Zamudio on 10/26/14.
+//  Copyright (c) 2014 zamudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
